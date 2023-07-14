9a7fd71c77b3425bd36d10dccdcccf1e3a928187
Qonda Mampofu <mampofuq@gmail.com>
2023-07-14 03:15:01 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char *msg = "\"Programming is like building a multilingual puzzle\n";
+	char *msg = "\"Programming is like building a multilingual puzzle";
 	puts(msg);
 	return (0);
 }
