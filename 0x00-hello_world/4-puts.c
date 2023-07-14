093cfa5efbb21077a5015878a61f55d5f28b7dd3
Qonda Mampofu <mampofuq@gmail.com>
2023-07-14 02:54:51 -0700

    print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	const char *msg = "Programming is like building a multilingual puzzle";
+	puts(msg);
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -5,3 +5,4 @@ Script on compiling C
 1-compiler compile a C file but doesn't not link.
 2-assembler generates  the assembly code of a C code.
 3-name compile and create executable output file.
+4-puts.c print string.
