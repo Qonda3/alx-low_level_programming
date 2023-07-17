80daa4f48cea60f12fd00e83fd5fc87c230b8a8a
Qonda Mampofu <mampofuq@gmail.com>
2023-07-17 13:08:00 -0700

    prints combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+	int m = 48;
+	int n = 48;
+
+	while (i <= 57)
+	{
+		while (j <= 57)
+		{
+			while (m <= 57)
+			{
+				while (n <= 57)
+				{
+					n += 1;
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(m);
+					putchar(n);
+					if (i != 57 && j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				n = j + 1;
+				m += 1;
+			}
+			m = i;
+			j += 1;
+		}
+		j = 48;
+		i += 1;
+	}
+	putchar('\n');
+	return (0);
+}
