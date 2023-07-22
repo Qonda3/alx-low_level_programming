0145f2f09589012e14d20223d1c22855cdc298a1
Qonda Mampofu <mampofuq@gmail.com>
2023-07-21 19:22:35 -0700

    prime factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,4 +7,22 @@
  */
 int main(void)
 {
-	
+	long int num = 612852475143;
+	int i = 2;
+	long int factor = 0;
+
+	while (num != 1)
+	{
+		if (num % i == 0)
+		{
+			while (num % i  == 0)
+			{
+				num = num / i;
+			}
+			factor = i;
+		}
+		i++;
+	}
+	printf("%ln", &factor);
+	return (0);
+}
