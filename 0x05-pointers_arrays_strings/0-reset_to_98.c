5e9423ed7c15fdb09d2a2e26f49b08e241d58ec4
Qonda Mampofu <mampofuq@gmail.com>
2023-07-25 13:12:25 -0700

    set integer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ * reset_to_98 - set integer to 98
+ * @n: a pointer the integer we want to set to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+Pointers and Arrays
+
+
+0-reset_to_98.c function that takes a pointer to an int as a parameter and updates the value it points to 98.
