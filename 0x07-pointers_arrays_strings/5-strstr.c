9cf1972fc5732bffc95e99b2171aed3b6f9f2b20
Qonda Mampofu <mampofuq@gmail.com>
2023-08-01 08:36:00 -0700

    locate a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,4 +6,33 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i = 0;
+	int count = 0;
+	int j, match;
+
+	while (needle[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		j = 0;
+		if (needle[j] == haystack[i])
+		{
+			match = 0;
+			while (needle[j] == haystack[i])
+			{
+				match++;
+				if (match == count)
+				{
+					count -= 1;
+					return (&(haystack[i - count]));
+				}
+				j++;
+				i++;
+			}
+		}
+	}
+	return ('\0');
 }
