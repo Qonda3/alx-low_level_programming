4b06fc2337f7b3aef50afb090a89fb1467fcac53
Qonda Mampofu <mampofuq@gmail.com>
2023-07-31 17:28:14 -0700

    print the chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,9 @@
+/**
+ * _strstr - locates a substring
+ * @haystack: a pointer to the string in which the search will be performed
+ * @needle: a pointer to the null-terminated substring.
+ * Return: a pointer to the first occurrence else NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -6,3 +6,5 @@ Functions on pointers, arrays and strings
 2-strchr.c locates a character in a string.
 3-strspn.c gets the length of a prefix substring.
 4-strpbrk.c searches a string for any of a set of bytes.
+5-strstr.c locates a substring.
+7-print_chessboard.c prints the chessboard.
