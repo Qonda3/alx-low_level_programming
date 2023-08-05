f84fa2cb83ee8f98439d644011f5d1104d534e4f
Qonda Mampofu <mampofuq@gmail.com>
2023-08-04 20:29:34 -0700

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+/**
+ * main - Entry point
+ * @argc: number of command-line arg passed.
+ * @argv: array of char pointers
+ * Return: integer
+ */
+int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[argc - 1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
