d40f98e1717b2f74f0342291b9b85d8558720a33
Qonda Mampofu <mampofuq@gmail.com>
2023-08-08 11:29:50 -0700

    copy the string given

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -24,7 +24,7 @@ char *_strdup(char *str)
 			count++;
 			i++;
 		}
-		newstr = malloc(count * sizeof(char));
+		newstr = malloc((count * sizeof(char)) + 1);
 		if (newstr == NULL)
 			return (NULL);
 		for (j = 0; j < count; j++)
