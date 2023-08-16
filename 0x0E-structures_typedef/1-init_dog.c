0888304957013e1e0a88c755300a10b0457b503e
Qonda Mampofu <mampofuq@gmail.com>
2023-08-15 19:50:46 -0700

    prints a struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,6 +11,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 	{
+		return (1);
 		(*d).name = NULL;
 		(*d).age = 0.00;
 		(*d).owner = NULL;
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -3,3 +3,5 @@ function on typedef & structures
 
 dog.h  define a new type struct dog.
 1-init_dog.c initialize a variable of type struct dog.
+2-print_dog.c  prints a struct dog.
+
