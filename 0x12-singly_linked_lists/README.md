34663629d4d6109f67bcb7709abe8cf050bf4489
Qonda Mampofu <mampofuq@gmail.com>
2023-08-24 18:10:25 -0700

    returns the number of elements

--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -2,3 +2,4 @@ Data Structures linked lists
 
 
 0-print_list.c - prints all the elements of a list_t list.
+1-list_len.c - returns the number of elements in a linked list_t list.
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
+#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,5 +17,6 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
