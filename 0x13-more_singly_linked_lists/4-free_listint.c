7580b9a9b8f2b918125218d9c25eab5262eddf23
Qonda Mampofu <mampofuq@gmail.com>
2023-08-30 06:38:50 -0700

    betty error

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+/**
+ * free_listint - frees
+ * @head: struct type
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	for (;head != NULL;)
+	for (; head != NULL;)
 	{
 		temp = head->next;
 		free(head);
