48b3f29cea159dfc0aa1dcf57b228eedef981132
Qonda Mampofu <mampofuq@gmail.com>
2023-08-30 06:32:04 -0700

    betty error

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
 typedef struct listint_s
 {
 	int n;
@@ -10,4 +18,5 @@ size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 #endif
