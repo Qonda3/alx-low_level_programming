fe2b94f58f6d247da96f49b758b2dfa804e6d336
Qonda Mampofu <mampofuq@gmail.com>
2023-08-31 19:52:32 -0700

    flpp

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one number to another.
+ * flip_bits - returns the number of bits.
  * @n: number
  * @m: number
  * Return: unsigned integer
@@ -8,11 +8,12 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i = 0, count = 0;
-	unsigned long int exclusive = n ^ m;
+	unsigned long int value;
 
+	value = n ^ m;
 	while (i <= 63)
 	{
-		count += (exclusive >> i) & 1;
+		count += (value >> i) & 1;
 		i++;
 	}
 	return (count);
