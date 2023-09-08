919ceb483d4aa48d29683ec79072f5d6702b114c
Qonda Mampofu <mampofuq@gmail.com>
2023-09-08 09:26:22 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,16 +18,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		close(file_d);
-		return (1);
-	}
-	while (text_content[num] != '\0')
-		num++;
-	w = write(file_d, text_content, num);
-	if (w == -1)
-	{
-		close(file_d);
-		return (-1);
+		while (text_content[num] != '\0')
+			num++;
+		w = write(file_d, text_content, num);
+		if (w == -1)
+		{
+			close(file_d);
+			return (-1);
+		}
 	}
 	close(file_d);
 	return (1);
