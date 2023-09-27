2a82435ceae37a38615343cdc52bd3d37afb2275
Qonda Mampofu <mampofuq@gmail.com>
2023-09-27 13:19:27 -0700

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	if (i == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
