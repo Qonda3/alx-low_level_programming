61f59f221089e6c57f8394acbbe87f372b84403f
Qonda Mampofu <mampofuq@gmail.com>
2023-09-30 06:18:40 -0700

    print the progam's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguments passed
+ * @argv: array of strings
+ * Return: (0) Success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+Arguments to main
+
+
+0-whatsmyname.c - programes that prints its name.
