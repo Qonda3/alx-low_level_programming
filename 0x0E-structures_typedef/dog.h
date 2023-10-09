f40c9a9fde75b3c9cdfe850bada74b310bcabe7c
Qonda Mampofu <mampofuq@gmail.com>
2023-10-09 14:53:43 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct dog type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
